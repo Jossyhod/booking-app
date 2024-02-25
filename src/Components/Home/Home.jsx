e9304acc97373d338bacc184835e25189e03d733
Joseph <ikeanyi_joseph@yahoo.com>
2024-02-25 21:15:52 +0100

    First Update

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Styles from './Home.module.css'
+
+function Home() {
+    return (
+        <div>
+          <div className={Styles.container}>
+            <Navbar />
+            <div>
+              
+            </div>
+          </div>
+        </div>
+      );
+}
+
+export default Home
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Styles from './Navbar.module.css'
+
+const Navbar = () => {
+    return (
+        <div className={Styles.container}>
+          <div className={Styles.logo}>
+            <img
+              src="https://res.cloudinary.com/dfeyofjln/image/upload/v1705255997/COLLEGE_QUARTERS_LOGO-removebg-preview_1_uxu3ds.png"
+              alt="Logo" width={"80px"}
+            />
+            <div className={Styles.line}></div>
+            <i><h5>Student <br/> Quarters</h5></i>
+          </div>
+          <div className={Styles.nav}>
+             <li>Residences</li>
+             <div className={Styles.line2}></div>
+             <li>Book Now</li>
+             <div className={Styles.line2}></div>
+             <li>Refer & Earn</li>
+             <div className={Styles.line2}></div>
+             <li>Testimonials</li>
+             <div className={Styles.line2}></div>
+             <li>Universities</li>
+             <div className={Styles.line2}></div>
+             <li>LOGIN</li>
+    
+          </div>
+        </div>
+      );
+}
+
+export default Navbar
