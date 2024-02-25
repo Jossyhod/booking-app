6bea65d357fcfeb3a1b53aced025559b1cddc6ac
Joseph <ikeanyi_joseph@yahoo.com>
2024-02-25 21:42:10 +0100

    updated

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,6 +8,13 @@ function Home() {
           <div className={Styles.container}>
             <Navbar />
             <div>
+                <img src="https://res.cloudinary.com/dfeyofjln/image/upload/v1692172726/contact_icon_mo8tpl.png" alt=""/>
+                <h3>Nigeria: +2348063343728 
+                International: +23480633437
+                   
+                </h3>
+            </div>
+            <div>
               
             </div>
           </div>
